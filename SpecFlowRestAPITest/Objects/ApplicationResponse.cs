fd1c556aa57598004463f4972620a8955ff69ee1
Venkat Rangala <venkatrangala@gmail.com>
2020-09-06 20:49:37 +0100

    Initial Commit BDD Specflow with RestSharp

---
+++ SpecFlowRestAPITest/Objects/ApplicationResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowRestAPITest.Objects
+{
+    public class ApplicationResponse
+    {
+        public string id { get; set; }
+        public string applicationReference { get; set; }
+        public string sessionToken { get; set; }
+        public DateTime applicationStartDate { get; set; }
+        public DateTime applicationLastUpdatedDate { get; set; }
+        public DateTime applicationSubmittedDate { get; set; }
+        public DateTime applicationCompletedDate { get; set; }
+        public string applicationStatus { get; set; }
+    }
+
+}
---
+++ SpecFlowRestAPITest/Utilities/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Configuration;
+
+namespace SpecFlowRestAPITest.Utilities
+{
+    public static class Data
+    {
+        //Imran Place end points here
+        public static string WeatherBaseURL => ConfigurationManager.AppSettings["WeatherBaseURL"];
+        public static string ZipCodeBaseURL => ConfigurationManager.AppSettings["ZipCodeBaseURL"];
+    }
+}
